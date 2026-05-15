6d7265360d4f63c7c459694abb36c2cca700dee1
Janghan Noh <janghan.noh@gm.gist.ac.kr>
2026-05-15 19:55:14 +0900

    Fix(WAIT/EXEC): use shared child status mechanism

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -537,14 +537,11 @@ init_thread (struct thread *t, const char *name, int priority)
   /* System Call */ 
   t->next_fd = 2; /* 0, 1 are reserved for stdin, stdout */
 
-  sema_init(&t->load_sema, 0);
-  t->load_success = false;
-
-  sema_init(&t->exit_sema, 0);
   t->exit_status = 0;
   t->executable = NULL;
 
-  sema_init(&t->free_sema, 0);
+  list_init(&t->children);
+  t->child_status = NULL;
   /* - */
 }
 
@@ -936,14 +933,4 @@ void thread_mlfqs_on_tick(bool per_sec, bool per_fourth) {
     list_sort(&ready_list, higher_thread_priority, NULL);
     thread_preemption();
   }
-}
-/* System Call */
-struct thread *get_thread_by_tid(tid_t tid) {
-  struct list_elem *e; 
-  /* iterate through all_list to find thread with given tid */
-  for(e = list_begin(&all_list); e != list_end(&all_list); e = list_next(e)) {
-    struct thread *t = list_entry(e, struct thread, allelem);
-    if (t->tid == tid) return t; 
-  } 
-  return NULL;
 }
--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -5,7 +5,6 @@
 #include <list.h>
 #include <stdint.h>
 #include "threads/fixed-point.h"
-#include "threads/synch.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -118,16 +117,11 @@ struct thread
     struct file *fdt[128];  /* file descriptor table, max 128 files per process*/
     int next_fd;            /* next available file descriptor */
 
-    struct semaphore
-        load_sema; /* sleeps parent until child is loaded */
-    bool load_success;
-
-    struct semaphore exit_sema; /* parent waits for child to exit */
-    bool exit_status;           /* stores the exit code */
-
-    struct semaphore free_sema;
-
+    int exit_status;           /* stores the exit code */
     struct file *executable;
+
+    struct list children;
+    struct child_status *child_status;
     
 
 #ifdef USERPROG
@@ -207,8 +201,5 @@ void thread_preemption(void);
 /* Multi-Level Feedback Queue Scheduler */
 void thread_mlfqs_on_tick(bool per_sec, bool per_fourth);
 /* end of Multi-Level Feedback Queue Scheduler */
-
-/* System Call */
-struct thread *get_thread_by_tid(tid_t tid);
 
 #endif /* threads/thread.h */
